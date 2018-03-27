c9d9649ca2f461dd86dd7f0008dbe58983a051f5
Sebastian Freundt <freundt@ga-group.nl>
2018-03-27 04:20:27 +0000

    minor, provide ABA RTN checking

--- src/numchk.h
+++ src/numchk.h
@@ -165,6 +165,9 @@ extern void nmpr_iposan(nmck_t, const char*, size_t);
 extern nmck_t nmck_npi(const char*, size_t);
 extern void nmpr_npi(nmck_t, const char*, size_t);
 
+extern nmck_t nmck_abartn(const char*, size_t);
+extern void nmpr_abartn(nmck_t, const char*, size_t);
+
 
 /* utils */
 static inline int ischeck(int c)
