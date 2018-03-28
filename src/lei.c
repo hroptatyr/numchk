70ed3b55258fa2a57a16526a6b14933294b8ccaf
Sebastian Freundt <freundt@ga-group.nl>
2018-03-28 08:34:51 +0000

    hygiene, make LEI checker stateless and avoid operating in ASCII space

--- src/lei.c
+++ src/lei.c
@@ -42,104 +42,76 @@
 #include "numchk.h"
 #include "nifty.h"
 
-typedef union {
-	nmck_t s;
-	struct {
-		short unsigned int pad;
-		char chk[2U];
-	};
-} lei_state_t;
-
-static lei_state_t
-calc_st(const char *str, size_t UNUSED(len))
+
+nmck_t
+nmck_lei(const char *str, size_t len)
 {
-	char buf[40U];
+	uint_fast8_t buf[40U];
 	size_t bsz = 0U;
-	unsigned int sum = 0U;
-	size_t j = 0U;
+	uint_fast32_t sum = 0U;
+	char chk[2U];
+
+	/* common cases first */
+	if (len != 20U) {
+		return -1;
+	}
 
 	/* expand string first */
 	for (size_t i = 0U; i < 18U; i++) {
 		switch (str[i]) {
 		case '0' ... '9':
-			buf[bsz++] = str[i];
-			j++;
+			buf[bsz++] = (unsigned char)(str[i] ^ '0');
 			break;
 		case 'A' ... 'J':
-			buf[bsz++] = '1';
-			buf[bsz++] = (char)((str[i] - 'A') ^ '0');
-			j++;
+			buf[bsz++] = 1U;
+			buf[bsz++] = (unsigned char)(str[i] - 'A');
 			break;
 		case 'K' ... 'T':
-			buf[bsz++] = '2';
-			buf[bsz++] = (char)((str[i] - 'K') ^ '0');
-			j++;
+			buf[bsz++] = 2U;
+			buf[bsz++] = (unsigned char)(str[i] - 'K');
 			break;
 		case 'U' ... 'Z':
-			buf[bsz++] = '3';
-			buf[bsz++] = (char)((str[i] - 'U') ^ '0');
-			j++;
+			buf[bsz++] = 3U;
+			buf[bsz++] = (unsigned char)(str[i] - 'U');
 			break;
 		default:
-			return (lei_state_t){0};
+			return -1;
 		}
 	}
 	/* and 00 */
-	buf[bsz++] = '0';
-	buf[bsz++] = '0';
+	buf[bsz++] = 0U;
+	buf[bsz++] = 0U;
 
 	/* now calc first sum */
-	sum = (buf[0U] ^ '0') * 10U + (buf[1U] ^ '0');
+	sum = buf[0U] * 10U + buf[1U];
 	for (size_t i = 2U; i < bsz; sum %= 97U) {
 		/* just so we calculate less modulos */
 		for (const size_t n = i + 7U < bsz ? i + 7U : bsz; i < n; i++) {
 			sum *= 10U;
-			sum += (buf[i] ^ '0');
+			sum += buf[i];
 		}
 	}
 
 	/* this is the actual checksum */
 	sum = 98U - sum;
+	chk[0U] = (char)((sum / 10U) ^ '0');
+	chk[1U] = (char)((sum % 10U) ^ '0');
 
-	return (lei_state_t){
-		.chk[0U] = (char)((sum / 10U) ^ '0'),
-		.chk[1U] = (char)((sum % 10U) ^ '0')
-	};
-}
-
-
-nmck_t
-nmck_lei(const char *str, size_t len)
-{
-	/* common cases first */
-	if (len != 20) {
-		return -1;
-	}
-
-	with (lei_state_t st = calc_st(str, len)) {
-		if (!st.s) {
-			return -1;
-		} else if (str[18U] != st.chk[0U] || str[19U] != st.chk[1U]) {
-			/* record state */
-			return st.s | 1;
-		}
-	}
-	/* all's good */
-	return 0;
+	/* all good, is it */
+	return (chk[0U] << 8U ^ chk[1U]) << 8U ^
+		(chk[0U] != str[18U] || chk[1U] != str[19U]);
 }
 
 void
 nmpr_lei(nmck_t s, const char *str, size_t len)
 {
-	lei_state_t st = {s};
-
-	if (LIKELY(!s)) {
+	if (LIKELY(!(s & 0b1U))) {
 		fputs("LEI, conformant with ISO 17442:2012", stdout);
 	} else if (s > 0 && len == 20U) {
 		fputs("LEI, not ISO 17442 conformant, should be ", stdout);
 		fwrite(str, sizeof(*str), 18U, stdout);
-		fputc(st.chk[0U], stdout);
-		fputc(st.chk[1U], stdout);
+		fputc(s >> 16U & 0x7fU, stdout);
+		fputc(s >> 8U & 0x7fU, stdout);
 	} else {
 		fputs("unknown", stdout);
 	}
